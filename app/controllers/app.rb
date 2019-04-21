885415440ca1ac8f5b6e76808a4def32a5867638
Hsiao-Tung Dong <s103060011@m103.nthu.edu.tw>
2019-04-21 12:05:07 +0800

    Write routes

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -3,51 +3,87 @@
 require 'roda'
 require 'json'
 
-require_relative '../models/diary'
-
 module CoEditPDF
   # Web controller for CoEditPDF API
   class Api < Roda
-    plugin :environments
     plugin :halt
-
-    configure do
-      Diary.setup
-    end
 
     route do |routing| # rubocop:disable Metrics/BlockLength
       response['Content-Type'] = 'application/json'
 
       routing.root do
-        { message: 'DiaryAPI up at /api/v1' }.to_json
+        { message: 'CoEditPDFAPI up at /api/v1' }.to_json
       end
 
-      routing.on 'api' do
-        routing.on 'v1' do
-          routing.on 'diary' do
-            # GET api/v1/diary/[id]
-            routing.get String do |id|
-              Diary.find(id).to_json
-            rescue StandardError
-              routing.halt 404, { message: 'Diary not found' }.to_json
-            end
+      @api_root = 'api/v1'
+      routing.on @api_root do
+        routing.on 'users' do
+          @user_route = "#{@api_root}/users"
 
-            # GET api/v1/diary
+          # GET api/v1/users
+          routing.get do
+            output = { data: User.all }
+            JSON.pretty_generate(output)
+          rescue StandardError
+            routing.halt 404, { message: 'Could not find users' }.to_json
+          end
+
+          # POST api/v1/users
+          routing.post do
+            new_data = JSON.parse(routing.body.read)
+            new_user = User.create(new_data)
+            raise('Could not save user') unless new_user
+
+            response.status = 201
+            response['Location'] = "#{@user_route}/#{new_user.id}"
+            { message: 'User saved', data: new_user }.to_json
+          rescue StandardError => error
+            routing.halt 400, { message: error.message }.to_json
+          end
+
+          routing.on String do |user_id|
+
+            # GET api/v1/users/[user_id]
             routing.get do
-              output = { diary_ids: Diary.all }
-              JSON.pretty_generate(output)
+              user = User.first(id: user_id)
+              user ? user.to_json : raise('User not found')
+            rescue StandardError => error
+              routing.halt 404, { message: error.message }.to_json
             end
 
-            # POST api/v1/diary
-            routing.post do
-              new_data = JSON.parse(routing.body.read)
-              new_doc = Diary.new(new_data)
+            routing.on 'pdfs' do
+              @pdf_route = "#{@api_root}/users/#{user_id}/pdfs"
 
-              if new_doc.save
-                response.status = 201
-                { message: 'Diary saved', id: new_doc.id }.to_json
-              else
-                routing.halt 400, { message: 'Could not save diary' }.to_json
+              # GET api/v1/users/[user_id]/pdfs
+              routing.get do
+                output = { pdf_ids: Pdf.all }
+                JSON.pretty_generate(output)
+              end
+  
+              # POST api/v1/users/[user_id]/pdfs
+              routing.post do
+                new_data = JSON.parse(routing.body.read)
+                user = User.first(id: user_id)
+                new_pdf = user.add_pdf(new_data)
+
+                if new_pdf
+                  response.status = 201
+                  response['Location'] = "#{@pdf_route}/#{new_pdf.id}"
+                  { message: 'PDF saved', data: new_pdf }.to_json
+                else
+                  routing.halt 400, 'Could not save pdf'
+                end
+                  
+              rescue StandardError
+                routing.halt 500, { message: 'Database error' }.to_json
+              end
+
+              # GET api/v1/users/[user_id]/pdfs/[pdf_id]
+              routing.get String do |pdf_id|
+                pdf = Pdf.where(user_id: user_id, id: pdf_id).first
+                pdf ? pdf.to_json : raise('PDF not found')
+              rescue StandardError => e
+                routing.halt 404, { message: e.message }.to_json
               end
             end
           end
