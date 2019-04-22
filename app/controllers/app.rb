d7f9200f7d2f4831af7270374ca7b2bcce6d3fc8
Hsiao-Tung Dong <s103060011@m103.nthu.edu.tw>
2019-04-22 11:43:56 +0800

    Pass specs
    Switch the order of routing.

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -20,43 +20,22 @@ module CoEditPDF
         routing.on 'users' do
           @user_route = "#{@api_root}/users"
 
-          # GET api/v1/users
-          routing.get do
-            output = { data: User.all }
-            JSON.pretty_generate(output)
-          rescue StandardError
-            routing.halt 404, { message: 'Could not find users' }.to_json
-          end
-
-          # POST api/v1/users
-          routing.post do
-            new_data = JSON.parse(routing.body.read)
-            new_user = User.create(new_data)
-            raise('Could not save user') unless new_user
-
-            response.status = 201
-            response['Location'] = "#{@user_route}/#{new_user.id}"
-            { message: 'User saved', data: new_user }.to_json
-          rescue StandardError => error
-            routing.halt 400, { message: error.message }.to_json
-          end
-
           routing.on String do |user_id|
-
-            # GET api/v1/users/[user_id]
-            routing.get do
-              user = User.first(id: user_id)
-              user ? user.to_json : raise('User not found')
-            rescue StandardError => error
-              routing.halt 404, { message: error.message }.to_json
-            end
 
             routing.on 'pdfs' do
               @pdf_route = "#{@api_root}/users/#{user_id}/pdfs"
 
+              # GET api/v1/users/[user_id]/pdfs/[pdf_id]
+              routing.get String do |pdf_id|
+                pdf = Pdf.where(user_id: user_id, id: pdf_id).first
+                pdf ? pdf.to_json : raise('PDF not found')
+              rescue StandardError => e
+                routing.halt 404, { message: e.message }.to_json
+              end
+
               # GET api/v1/users/[user_id]/pdfs
               routing.get do
-                output = { pdf_ids: Pdf.all }
+                output = { data: User.first(id: user_id).pdfs }
                 JSON.pretty_generate(output)
               end
   
@@ -77,15 +56,36 @@ module CoEditPDF
               rescue StandardError
                 routing.halt 500, { message: 'Database error' }.to_json
               end
-
-              # GET api/v1/users/[user_id]/pdfs/[pdf_id]
-              routing.get String do |pdf_id|
-                pdf = Pdf.where(user_id: user_id, id: pdf_id).first
-                pdf ? pdf.to_json : raise('PDF not found')
-              rescue StandardError => e
-                routing.halt 404, { message: e.message }.to_json
-              end
             end
+
+            # GET api/v1/users/[user_id]
+            routing.get do
+              user = User.first(id: user_id)
+              user ? user.to_json : raise('User not found')
+            rescue StandardError => error
+              routing.halt 404, { message: error.message }.to_json
+            end
+          end
+
+          # GET api/v1/users
+          routing.get do
+            output = { data: User.all }
+            JSON.pretty_generate(output)
+          rescue StandardError
+            routing.halt 404, { message: 'Could not find users' }.to_json
+          end
+
+          # POST api/v1/users
+          routing.post do
+            new_data = JSON.parse(routing.body.read)
+            new_user = User.create(new_data)
+            raise('Could not save user') unless new_user
+
+            response.status = 201
+            response['Location'] = "#{@user_route}/#{new_user.id}"
+            { message: 'User saved', data: new_user }.to_json
+          rescue StandardError => error
+            routing.halt 400, { message: error.message }.to_json
           end
         end
       end
