fb0e675d58fb7e2cdbe64a4aad2efe7e85c882b6
u10000129 <u10000129@gmail.com>
2019-05-16 10:10:13 +0800

    Heroku: specify foreign type in join table

--- app/db/migrations/03_accounts_pdfs_join_create.rb
+++ app/db/migrations/03_accounts_pdfs_join_create.rb
@@ -4,6 +4,12 @@ require 'sequel'
 
 Sequel.migration do
   change do
-    create_join_table(collaborator_id: :accounts, pdf_id: :pdfs)
+    # create_join_table(collaborator_id: :accounts, pdf_id: :pdfs)
+    create_table(:accounts_pdfs) do
+      foreign_key :collaborator_id, :accounts, type: 'uuid'
+      foreign_key :pdf_id, :pdfs, type: 'uuid'
+      primary_key [:collaborator_id, :pdf_id]
+      index [:collaborator_id, :pdf_id]
+    end
   end
 end
