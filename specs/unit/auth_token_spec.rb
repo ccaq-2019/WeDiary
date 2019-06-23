9d554681491fb5d95e195f1ab4decb5d336ed317
Lisa Dong <lisa.dong@gapp.nthu.edu.tw>
2019-06-23 11:41:19 +0800

    Add test annotation to pdfs_spec.rb

--- specs/unit/auth_token_spec.rb
+++ specs/unit/auth_token_spec.rb
@@ -7,21 +7,21 @@ describe 'Test Auth Token' do
     @payload = { name: 'Angels', email: 'angels@mlb.com' }
   end
 
-  it 'should return the correct contents' do
+  it 'HAPPY: should return the correct contents' do
     token = AuthToken.create(@payload)
     contents = AuthToken.contents(token)
 
     _(contents['payload'].to_json).must_equal @payload.to_json
   end
 
-  it 'should raise ExpiredTokenError if the token is expired' do
+  it 'SAD: should raise ExpiredTokenError if the token is expired' do
     token = AuthToken.create(@payload, AuthScope.new, 0)
     proc {
       AuthToken.contents(token)
     }.must_raise AuthToken::ExpiredTokenError
   end
 
-  it 'should raise InvalidTokenError if the token is invalid' do
+  it 'BAD: should raise InvalidTokenError if the token is invalid' do
     proc {
       AuthToken.contents('fake_token')
     }.must_raise AuthToken::InvalidTokenError
